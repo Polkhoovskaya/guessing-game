0b329d0d8863f63588942b46e1e6c62f3256629c
Volha <polkhovskayaolga@mail.ru>
2019-10-13 12:45:44 +0400

    feat: done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minValue = null;
+        this.maxValue = null;
+        this.result = null;
+    }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        this.result = Math.round((this.maxValue + this.minValue) / 2);
+        return this.result;
     }
 
     lower() {
-
+        this.maxValue = this.result;
     }
 
     greater() {
-
+        this.minValue = this.result;
     }
 }
 
